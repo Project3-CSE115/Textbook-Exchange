27a9e6fe83662adc6d8198260829b45d26050fcf
Priyonti Tabassum Haque <priyonti.haque.251@northsouth.edu>
2025-12-20 15:09:41 +0600

    Refactor User class and add toTxt method
    Updated User class methods and added toTxt method for serialization.

--- User.java
+++ User.java
@@ -2,38 +2,37 @@ package BookExchangeApp;
 
 import java.io.Serializable;
 
-public abstract class User implements Serializable{
-	protected String username;
-	protected String ID;
-	protected String password;
-	protected String email;
-	protected String contact;	
-	
-	public User(String username, String ID, String password, String email, String contact) {
-		this.username = username;
-		this.ID = ID;
-		this.password = password;
-		this.email = email;
-		this.contact = contact;
-	}
-	
-	public String getUsername() { return username; }
-	public String getID() { return ID; }
-	public String getEmail() { return email; }
-	public String getContact() { return contact; }
-	
-	public void setPassword(String password) { this.password = password; }
-	public void setUsername(String username) { this.username = username; }
-	public void setContact(String contact) { this.contact = contact;}
-	
-	public abstract void displayUserProfile();
-	
-	@Override
-	public String toString() {
-		return "Name: "+ this.username+
-				"/nID: "+ this.ID+
-				"/nEmail: "+this.email+
-				"/nPassword: "+this.password+
-				"/nContact: "+this.contact;
-	}
+public abstract class User implements Serializable {
+    protected String username;
+    protected String ID;
+    protected String password;
+    protected String email;
+    protected String contact;    
+
+    public User(String username, String ID, String password, String email, String contact) {
+        this.username = username;
+        this.ID = ID;
+        this.password = password;
+        this.email = email;
+        this.contact = contact;
+    }
+
+    public String getName() { return username; }
+    public String getID() { return ID; }
+    public String getEmail() { return email; }
+    public String getContact() { return contact; }
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+    public void setUsername(String username) { this.username = username; }
+    public void setContact(String contact) { this.contact = contact; }
+
+    public abstract void displayUserProfile();
+
+    public String toTxt() {
+        //Format: Type|Name|ID|Password|Email|Contact
+        String type = (this instanceof Seller) ? "Seller" : "Student";
+        return type + "|" + username + "|" + ID + "|" + password + "|" + email + "|" + contact;
+    }
 }
+
